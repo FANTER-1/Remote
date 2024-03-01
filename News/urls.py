4cb552cc6f9aa4a334441ccc8081032ea2a7fa54
FANTER-1 <147248849+FANTER-1@users.noreply.github.com>
2024-03-01 16:20:31 +0300

    Add files via upload

--- News/urls.py
+++ News/urls.py
@@ -1,12 +1,26 @@
-from django.urls import path
-from . import views
-from django.contrib.auth.views import LoginView
+"""
+URL configuration for project1 project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 
 urlpatterns = [
-    path('news/', views.news_list, name='news_list'),
-    path('news/<int:news_id>/', views.news_detail, name='news_detail'),
-
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    ...
+    path('admin/', admin.site.urls),
+    path('pages/', include('django.contrib.flatpages.urls')),
+    path('products/', include('simpleapp.urls')),
+    path('news/', include('News.urls')),
 ]
