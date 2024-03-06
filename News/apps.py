be8cfe04a886f4ace7cc9ca540fb9fc1590deb7a
FANTER-1 <147248849+FANTER-1@users.noreply.github.com>
2024-03-06 22:01:33 +0300

    Update apps.py

--- News/apps.py
+++ News/apps.py
@@ -4,3 +4,20 @@ from django.apps import AppConfig
 class NewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'News'
+class AppointmentConfig(AppConfig):
+    name = 'appointment'
+
+    def ready(self):
+        import appointment.signal
+
+        from .tasks import send_mails
+        from .scheduler import appointment_scheduler
+        print('started')
+
+        appointment_scheduler.add_job(
+            id='mail send',
+            func=send_mails,
+            trigger='interval',
+            seconds=10,
+        )
+        appointment_scheduler.start()
